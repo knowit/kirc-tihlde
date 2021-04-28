f79d6fa00eccb6ee3250c63a4884c30199ec6051
Henning M. Wold <henwol@knowit.no>
2021-04-28 19:08:52 +0200

    Gruppe 5: Løser oppgaven WOOOOOOO!

--- endpoint/endpoint.js
+++ endpoint/endpoint.js
@@ -1,6 +1,7 @@
 "use strict";
 const AWS = require("aws-sdk");
 const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 AWS.config.update({ region: "eu-central-1" });
 
@@ -13,14 +14,12 @@ module.exports.send = async (event) => {
   const params = {
     MessageBody: message,
     QueueUrl: process.env.QUEUE_URL,
-    MessageGroupId: "Message"
-  }
-  console.log("params", params)
+    MessageGroupId: "Message",
+  };
+  console.log("params", params);
   var statusCode, result;
   try {
-    await sqs
-      .sendMessage(params)
-      .promise();
+    await sqs.sendMessage(params).promise();
     statusCode = 201;
     result = "Message accepted";
   } catch (error) {
@@ -33,5 +32,19 @@ module.exports.send = async (event) => {
     body: JSON.stringify({
       message: result,
     }),
+  };
+};
+
+module.exports.getMessages = async (event) => {
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+  };
+
+  const messages = await dynamodb.scan(params, (err, data) => err ? err : data).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(messages["Items"].map(item => AWS.DynamoDB.Coverter.unmarshall(item)))
   };
 };
