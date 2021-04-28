0a617495771d404aec8b8d315a87871f1c4080d2
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2021-04-28 17:22:46 +0200

    Added *barebones* style support

--- cli/message_client.py
+++ cli/message_client.py
@@ -11,13 +11,18 @@ def main():
         print()
         print("Skriv inn meldingen du ønsker å sende til køen. For at meldingen faktisk skal bli sendt må du trykke på enter-tasten.")
         user_input = input()
+
+        print("Skriv inn CSS separert med semikolon:")
+        style = input()
+
         id = str(uuid.uuid4())
         timestamp = datetime.now(timezone.utc).astimezone().isoformat()
         msg = {
             "message": user_input, 
             "id": id, 
-            "timestamp": timestamp, 
-            }
+            "timestamp": timestamp,
+            "style": style,
+        }
         resp = requests.post('https://5wjbztusyb.execute-api.eu-central-1.amazonaws.com/dev/messages', json=msg)
         print("Melding sendt!")
         
