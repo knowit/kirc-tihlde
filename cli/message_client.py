e798632a1f9dede2b0ac373423ead91397e58b85
xyw089 <mittnavnerkult@gmail.com>
2021-04-28 17:49:01 +0200

    test

--- cli/message_client.py
+++ cli/message_client.py
@@ -3,21 +3,39 @@ import uuid
 import json
 import requests
 
+default_style = {
+    "font-color": "black",
+    "background-color": "white",
+    "font-weight": "bold",
+    "font-style": "italic",
+    "text-decoration": "underline",
+    "text-decoration-line": "line-through",
+    "background-image": ""
+}
 
 def main():
     print("Velkommen til verdens enkleste meldingsklient.")
     print("Klienten kan avsluttes når som helst ved å holde inne CTRL + C.")
     while True:
         print()
-        print("Skriv inn meldingen du ønsker å sende til køen. For at meldingen faktisk skal bli sendt må du trykke på enter-tasten.")
-        user_input = input()
+        print("Skriv inn meldingen du ønsker å sende til køen. For at meldingen faktisk skal bli sendt må du trykke på enter-tasten. For å legge til styling skriv '/style' etterfulgt av ønsket styling. Skrives med , for å skille hvert styling element.")
+        user_input = input().split('/style')
+        message = user_input[0].strip()
+        style_input = user_input[1].split(',')
+        for style in style_input:
+            split_style = style.split(':')
+            key = split_style[0].strip()
+            value = split_style[1].strip()
+            default_style[key] = value
         id = str(uuid.uuid4())
         timestamp = datetime.now(timezone.utc).astimezone().isoformat()
         msg = {
-            "message": user_input, 
+            "message": message, 
             "id": id, 
             "timestamp": timestamp, 
+            "style": default_style,
             }
+        print(msg)
         resp = requests.post('https://5wjbztusyb.execute-api.eu-central-1.amazonaws.com/dev/messages', json=msg)
         print("Melding sendt!")
         
--- processor/processor.js
+++ processor/processor.js
@@ -11,12 +11,22 @@ async function processor(event, context) {
 
 const processMessage = function (messageAsString) {
   const message = JSON.parse(messageAsString)
+
+  json_style = JSON.parse(message.style);
+  let style_output = '';
+  // for (const [key, value] of Object.entries(json_style)){
+  //   style_output += `${key}: ${value};`
+  // }
+  json_style.forEach(e => {
+    style_output += `${e[key]}: ${e[value]};`    
+  });
+
   return {
     message: message.message,
     timestamp: new Date().toISOString(),
     id: message.id,
     nickname: message.nickname,
-    style: message.style,
+    style: style_output,
   }
 }
 
