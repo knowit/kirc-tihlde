10f8d2655c2a16f556d6098e7610f6bb3e606e1a
pettelau <pettelau@stud.ntnu.no>
2021-04-28 19:04:17 +0200

    gruppe 4

--- endpoint/endpoint.js
+++ endpoint/endpoint.js
@@ -1,6 +1,7 @@
 "use strict";
 const AWS = require("aws-sdk");
 const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const dynamoDB = new AWS.DynamoDB();
 
 AWS.config.update({ region: "eu-central-1" });
 
@@ -32,6 +33,71 @@ module.exports.send = async (event) => {
     statusCode: statusCode,
     body: JSON.stringify({
       message: result,
+    }),
+  };
+};
+
+module.exports.get = async (event) => {
+  console.log("event", event);
+  const message = JSON.stringify(event.body);
+  console.log("message", message);
+  // TODO:  validate event
+
+  var params = {TableName: "kirc"}
+  var statusCode, result;
+  dynamoDB
+      .scan(params, (err, data) => {
+        if (err) {
+          statusCode = 500
+          result = err;
+        }
+        else {
+          statusCode = 201
+          result = data;
+        }  
+      })
+
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      messages: result,
+    }),
+  };
+};
+
+module.exports.getSince = async (event) => {
+  var sinceTime = event.query.from
+  console.log("event", event);
+  const message = JSON.stringify(event.body);
+  console.log("message", message);
+  // TODO:  validate event
+
+  var params = {TableName: "kirc", ComparisonOperator: aws.String("BETWEEN"),
+  AttributeValueList: []*dynamodb.AttributeValue {
+     {
+        S: aws.String(sinceTime),
+     },
+     {
+        S: aws.String(date.now().toISOString()),
+     },
+  },}
+  var statusCode, result;
+  dynamoDB
+      .scan(params, (err, data) => {
+        if (err) {
+          statusCode = 500
+          result = err;
+        }
+        else {
+          statusCode = 201
+          result = data;
+        }  
+      })
+
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      messages: result,
     }),
   };
 };
